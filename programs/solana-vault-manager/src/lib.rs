bad0ef2af8e562059fbc267d2c2bf44751e806e2
Jorge Hernandez <jdaviderb@gmail.com>
2023-09-25 19:55:14 -0500

    add create and withdraw instruction

--- programs/solana-vault-manager/src/lib.rs
+++ programs/solana-vault-manager/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
 declare_id!("Ggcpkb5z4ZKseSnfJ3YbCZ9UnLeiiUmEcu6K89xdqaU9");
 
@@ -6,10 +7,88 @@ declare_id!("Ggcpkb5z4ZKseSnfJ3YbCZ9UnLeiiUmEcu6K89xdqaU9");
 pub mod solana_vault_manager {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_vault(ctx: Context<CreateVaultContext>, seed_vault: Vec<u8>) -> Result<()> {
+        ctx.accounts.vault.owner = ctx.accounts.owner.key();
+        ctx.accounts.vault.mint = ctx.accounts.mint.key();
+        ctx.accounts.vault.seed = seed_vault;
+        ctx.accounts.vault.bump = *ctx.bumps.get("vault").unwrap();
+
+        Ok(())
+    }
+
+    pub fn withdraw_vault(ctx: Context<TransferContext>, amount: u64) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.vault_token.to_account_info(),
+            to: ctx.accounts.destination.to_account_info(),
+            authority: ctx.accounts.vault.to_account_info(),
+        };
+
+        let ctx_transfer =CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+        token::transfer(
+            ctx_transfer.with_signer(&[&&[
+                ctx.accounts.vault.seed.as_slice(),
+                &[ctx.accounts.vault.bump],
+            ][..]]),
+            amount,
+        )?;
+  
         Ok(())
     }
 }
 
+#[account]
+pub struct Vault {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub seed: Vec<u8>,
+    pub bump: u8,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(seed_vault: Vec<u8>)]
+pub struct CreateVaultContext<'info> {
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init, 
+        payer = owner, 
+        space = 8 + 32 + 32 + 1 + 4 + seed_vault.len(), 
+        seeds = [seed_vault.as_slice()], 
+        bump
+    )]
+    pub vault: Account<'info, Vault>,
+    #[account(
+        init,
+        payer = owner,
+        token::mint = mint,
+        token::authority = vault,
+        seeds = [ b"vault".as_ref(), vault.key().as_ref()],
+        bump
+    )]
+    pub vault_token: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+
+#[derive(Accounts)]
+pub struct TransferContext<'info> {
+    pub mint: Account<'info, Mint>,
+    #[
+        account(
+            constraint = owner.key() == vault.owner,
+            constraint = mint.key() == vault.mint
+        )
+    ]
+    #[account(mut)]
+    pub vault: Account<'info, Vault>,
+    #[account(mut, seeds = [b"vault".as_ref(), vault.key().as_ref()], bump)]
+    pub vault_token: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub destination: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
--- tests/solana-vault-manager.ts
+++ tests/solana-vault-manager.ts
@@ -1,16 +1,174 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaVaultManager } from "../target/types/solana_vault_manager";
+import CreateToken from './utils/token';
+import { assert } from "chai";
+
+const { TOKEN_PROGRAM_ID, createTransferInstruction } = require('@solana/spl-token');
 
 describe("solana-vault-manager", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  const payer = anchor.web3.Keypair.fromSecretKey(
+    Buffer.from(
+      JSON.parse(
+        require("fs").readFileSync(process.env.ANCHOR_WALLET, {
+          encoding: "utf-8",
+        })
+      )
+    )
+  );
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaVaultManager as Program<SolanaVaultManager>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("#create_vault", async () => {
+
+    const token = await CreateToken({
+        inputs: {
+          connection: provider.connection,
+          amount: 1000,
+        },
+        accounts: {
+          payerSign: payer,
+          payer: payer.publicKey,
+        },
+    });
+
+    const [player] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from('vault'), 
+        anchor.web3.Keypair.generate().publicKey.toBuffer(), 
+        Buffer.from('player'),
+        anchor.web3.Keypair.generate().publicKey.toBuffer()
+      ], 
+      program.programId
+    );
+    
+    const seed = player.toBuffer();
+    const [vault] = anchor.web3.PublicKey.findProgramAddressSync(
+      [seed], 
+      program.programId
+    );
+
+    const [vaultToken] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from('vault'), vault.toBuffer()], 
+      program.programId
+    );
+    
+    await program.methods.createVault(seed).accounts({
+      vault,
+      mint: token.accounts.mint,
+      owner: provider.wallet.publicKey,
+      vaultToken: vaultToken
+    }).rpc();
+
+
+    assert.equal(
+      (await provider.connection.getTokenAccountBalance(vaultToken)).value.amount,
+      '0',
+      'vaultToken balance should be 0'
+    );
+
+    assert.equal(
+      (await provider.connection.getTokenAccountBalance(token.accounts.payerMintAccount)).value.amount,
+      '1000',
+      'payer balance should be 1000;'
+    );
+  });
+
+  it("#withdrawl", async () => {
+
+    const INIT_AMOUNT = 1000;
+    const token = await CreateToken({
+        inputs: {
+          connection: provider.connection,
+          amount: INIT_AMOUNT,
+        },
+        accounts: {
+          payerSign: payer,
+          payer: payer.publicKey,
+        },
+    });
+
+    const [player] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from('vault'), 
+        anchor.web3.Keypair.generate().publicKey.toBuffer(), 
+        Buffer.from('player'),
+        anchor.web3.Keypair.generate().publicKey.toBuffer()
+      ], 
+      program.programId
+    );
+    
+    const seed = player.toBuffer();
+    const [vault] = anchor.web3.PublicKey.findProgramAddressSync(
+      [seed], 
+      program.programId
+    );
+
+    const [vaultToken] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from('vault'), vault.toBuffer()], 
+      program.programId
+    );
+    
+    await program.methods.createVault(seed).accounts({
+      vault,
+      mint: token.accounts.mint,
+      owner: provider.wallet.publicKey,
+      vaultToken: vaultToken
+    }).rpc();
+
+    // transfer funds to vault
+
+    const transaction = new anchor.web3.Transaction();
+    transaction.add(
+      createTransferInstruction(
+        token.accounts.payerMintAccount,
+        vaultToken,
+        provider.wallet.publicKey,
+        INIT_AMOUNT,
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    transaction.feePayer = provider.wallet.publicKey;
+    transaction.recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash;
+    
+    await anchor.web3.sendAndConfirmTransaction(provider.connection, transaction, [
+      payer,
+    ]);
+
+    assert.equal(
+      (await provider.connection.getTokenAccountBalance(vaultToken)).value.amount,
+      '1000',
+      'vaultToken balance should be 1000;'
+    );
+
+    assert.equal(
+      (await provider.connection.getTokenAccountBalance(token.accounts.payerMintAccount)).value.amount,
+      '0',
+      'payer balance should be 0;'
+    );
+
+    await program.methods.withdrawVault(new anchor.BN(1000)).accounts({
+      vault,
+      mint: token.accounts.mint,
+      owner: provider.wallet.publicKey,
+      vaultToken: vaultToken,
+      destination: token.accounts.payerMintAccount
+    }).rpc();
+
+    assert.equal(
+      (await provider.connection.getTokenAccountBalance(vaultToken)).value.amount,
+      '0',
+      'vaultToken balance should be 0'
+    );
+
+    assert.equal(
+      (await provider.connection.getTokenAccountBalance(token.accounts.payerMintAccount)).value.amount,
+      '1000',
+      'payer balance should be 1000'
+    );
   });
 });
